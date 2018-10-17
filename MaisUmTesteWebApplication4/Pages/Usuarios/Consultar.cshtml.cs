0b4644ae34e1a5f2ecff14c36493bdb30e626eec
1710492 <1710492@DASOE671C6>
2018-10-16 22:17:18 -0300

    Alteração lista

--- MaisUmTesteWebApplication4/Pages/Usuarios/Consultar.cshtml.cs
+++ MaisUmTesteWebApplication4/Pages/Usuarios/Consultar.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MaisUmTesteWebApplication4.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,6 +13,19 @@ namespace MaisUmTesteWebApplication4.Pages.Usuarios
         public void OnGet()
         {
 
+        }
+        [HttpPost]
+
+        public void OnPost(int cpf)
+        {
+            List<Usuario> usuarios = CadastrarModel.usuarios;
+            foreach (Usuario u in usuarios)
+            {
+                if (cpf.Equals(u.Cpf))
+                {
+                    ViewData["usuario"] = u;
+                }
+            }
         }
     }
 }
